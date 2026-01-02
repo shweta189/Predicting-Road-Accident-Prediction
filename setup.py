ab457c3e57f2c7b3eec5d307baf522a9ddf3afdb
shweta yadav <shwetarayadav@gmail.com>
2026-01-02 21:37:52 +0530

    setting up the project configuration

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Shweta Yadav",
+    author_email="shery40123@.com",
+    packages=find_packages()
+)
